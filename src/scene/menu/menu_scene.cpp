59f6802e96cb9e7490812708199455aaf5eb197e
Hani <hani@sei.ir>
2016-09-03 12:34:29 +0430

    Menu keyboard navigation #9

--- src/scene/menu/menu_scene.cpp
+++ src/scene/menu/menu_scene.cpp
@@ -13,5 +13,34 @@ menu_scene::menu_scene() : selected_index(0)
 
 std::wstring menu_scene::update(const double delta, const crib::input::buffer& input)
 {
+	for (auto& e : input)
+	{
+		if (e.message == WM_KEYDOWN)
+		{
+			switch (e.wParam)
+			{
+			case VK_DOWN:
+				if (selected_index == items.size() - 1) selected_index = 0;
+				else if (selected_index < items.size()) selected_index++;
+				break;
+
+			case VK_UP:
+				if (selected_index == 0) selected_index = items.size() - 1;
+				else selected_index--;
+				break;
+
+			case VK_ESCAPE:
+				if (selected_index == items.size() - 1) return L"quit";
+				else selected_index = items.size() - 1;
+				break;
+
+			case VK_SPACE:
+			case VK_RETURN:
+				if (items[selected_index].action.size()) return items[selected_index].action;
+				break;
+			}
+		}
+	}
+
 	return L"";
 }
