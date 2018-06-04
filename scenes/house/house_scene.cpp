736abd250102857ce7ddd0e19caa3a036b82ef51
Hani <hani@hnkb.io>
2018-06-04 16:50:12 +0430

    Add more objects to the scene

--- scenes/house/house_scene.cpp
+++ scenes/house/house_scene.cpp
@@ -13,10 +13,14 @@ scene::scene(crib::core::settings& settings) : scene_3d(settings), camera_contro
 	entities.emplace_back(L"earth", L"earth", L"basic");
 	entities.emplace_back(L"cube0", L"cube", L"basic");
 	entities.emplace_back(L"cube1", L"cube", L"basic");
+	entities.emplace_back(L"cube2", L"cube", L"basic");
+	entities.emplace_back(L"pyramid0", L"pyramid", L"basic");
 
-	camera.position = XMVectorSet(0, 1, 0, 0);
-	entities[1].world_transform = XMMatrixTranslation(20, .5f, 40);
-	entities[2].world_transform = XMMatrixTranslation(-10, .5f, 30);
+	camera.position = XMVectorSet(0, 2.f, 0, 0);
+	entities[1].world_transform = XMMatrixScaling(2.f, 2.f, 2.f) * XMMatrixTranslation(20.f, 1.f, 40.f);
+	entities[2].world_transform = XMMatrixTranslation(-10.f, .5f, 30.f);
+	entities[3].world_transform = XMMatrixScaling(4.f, 4.f, 2.f) * XMMatrixTranslation(0, 2.f, -35.f);
+	entities[4].world_transform = XMMatrixScaling(1.f, 2.f, 2.f) * XMMatrixTranslation(10.f, .5f, -30.f);
 
 	light.direction = XMFLOAT3(XMVector3Normalize(XMVectorSet(-.2f, 1.f, -.5f, 0.f)).m128_f32);
 	light.ambient = XMFLOAT4(.2f, .1f, .1f, 1.f);
