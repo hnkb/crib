2d21aa33976ba8f41a1b704498b0ac131eeff7ea
Hani Kaabi <hani@miro.com>
2023-08-27 09:14:53 +0200

    Also use addrinfo for server addresses

--- src/Network/Socket.cpp
+++ src/Network/Socket.cpp
@@ -1,5 +1,9 @@
 
 #include <crib/Network>
+#include <iostream>
+// #include <sys/types.h>
+// #include <sys/socket.h>
+#	include <unistd.h>
 
 using crib::Socket;
 
@@ -7,6 +11,71 @@ using crib::Socket;
 Socket::Socket(int domain, int type, int protocol)
 {
 	fd = socket(domain, type, protocol);
+	if (fd == -1)
+		throw lastError();
+}
+
+Socket::Socket(const char* nodeName, const char* serviceName, bool isPassive, int type)
+{
+	addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = type;
+	// hints.ai_flags = AI_CANONNAME;
+	// hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = isPassive ? 0 : AI_PASSIVE;
+
+	addrinfo* result = nullptr;
+	auto ret = getaddrinfo(nodeName, serviceName, &hints, &result);
+	if (ret != 0)
+	{
+		printf(
+			"getaddrinfo failed with error: %d\n%s\n\n",
+			ret,
+			std::system_error(std::error_code(ret, std::generic_category())).what());
+		return;
+	}
+
+	for (auto ptr = result; ptr != nullptr; ptr = ptr->ai_next)
+	{
+		std::cout << "Family: " << ptr->ai_family << ", Protocol: " << ptr->ai_protocol
+				  << ", Address length: " << ptr->ai_addrlen << "\n";
+
+		char buf[INET6_ADDRSTRLEN], ser[500];
+		// inet_ntop(ptr->ai_addr->sa_family, ptr->ai_addr->sa_data, buf, sizeof(buf));
+		getnameinfo(
+			ptr->ai_addr,
+			ptr->ai_addrlen,
+			buf,
+			sizeof(buf),
+			ser,
+			sizeof(ser),
+			0 | NI_NUMERICHOST | NI_NUMERICSERV);
+		std::cout << "   " << buf << "         " << ser << "\n";
+		if (ptr->ai_canonname)
+			std::cout << "   " << ptr->ai_canonname << "\n";
+
+		fd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		if (fd == -1)
+			throw lastError();
+
+		int iResult;
+		if (isPassive)
+			iResult = bind(fd, ptr->ai_addr, ptr->ai_addrlen);
+		else
+			iResult = connect(fd, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult < 0)
+		{
+			// closesocket(fd);
+			close(fd);
+			fd = -1;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
 	if (fd == -1)
 		throw lastError();
 }
