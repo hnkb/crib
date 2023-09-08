a0df65f39e550d65e891096fe10be76cdb41da61
Hani Kaabi <hani@hnkb.de>
2023-09-09 01:16:44 +0200

    Create a custom window for hello2d example

--- examples/hello2d/main.cpp
+++ examples/hello2d/main.cpp
@@ -2,13 +2,32 @@
 #include <crib/App>
 #include <iostream>
 
+using namespace crib;
+
+
+class MyWindow : public App::Window
+{
+public:
+	void onSizeChanged(int2 dims) override
+	{
+		Window::onSizeChanged(dims);
+		printf("size\n");
+	}
+	void onPosChanged(int2 pos) override
+	{
+		Window::onPosChanged(pos);
+		printf("move\n");
+	}
+};
 
 int main()
 {
 	try
 	{
-		crib::App::Window win;
-		return crib::App::run();
+		// App::Window win;
+		MyWindow win2;
+		auto win = std::move(win2);
+		return App::run();
 	}
 	catch (std::exception& ex)
 	{
