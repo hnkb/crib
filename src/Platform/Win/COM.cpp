600b4e095b1681eb7fbdbbd6f6e43ff98c86f636
Hani Kaabi <hani@hnkb.de>
2023-09-24 22:37:30 +0200

    Error handling utilities for Windows

---
+++ src/Platform/Win/COM.cpp
@@ -0,0 +1,30 @@
+
+#include <crib/Platform/Win>
+#include <objbase.h>
+
+using namespace crib::Platform::Win;
+
+
+InitializeCOM::InitializeCOM() : InitializeCOM(COINIT_MULTITHREADED)
+{}
+
+InitializeCOM::InitializeCOM(const DWORD concurrencyModel)
+{
+	// If CoInitializeEx fails, but only because COM has already been initialized with a
+	// different concurrency model, we don't want to throw an exception, because COM is
+	// available. The only difference is, in that case we don't want to uninitialize in
+	// destructor.
+
+	const auto errorCode = CoInitializeEx(nullptr, concurrencyModel);
+
+	mustShutdown = SUCCEEDED(errorCode);
+
+	if (!mustShutdown && errorCode != RPC_E_CHANGED_MODE)
+		throw Error(errorCode);
+}
+
+InitializeCOM::~InitializeCOM()
+{
+	if (mustShutdown)
+		CoUninitialize();
+}
