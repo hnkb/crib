99b2500e4aa3662feea0caeac0d2971d292ae622
Hani Kaabi <hani@hnkb.de>
2023-09-25 02:11:23 +0200

    (wip) render font

---
+++ src/Graphics/Font.cpp
@@ -0,0 +1,34 @@
+
+#include "Font.h"
+#include <stdexcept>
+
+using crib::Font;
+
+Font::Font(const std::filesystem::path& filename)
+{
+	ttf_load_from_file(filename.u8string().c_str(), &font, false);
+	if (!font)
+		throw std::runtime_error("Unable to load font");
+}
+
+Font::Glyph::~Glyph()
+{
+	if (mesh)
+		ttf_free_mesh(mesh);
+}
+
+Font::Glyph Font::getGlyph(wchar_t symbol)
+{
+	auto index = ttf_find_glyph(font, symbol);
+	if (index < 0)
+		throw std::invalid_argument("glyph not found in the font");
+
+	Glyph ret;
+	ret.glyph = &font->glyphs[index];
+
+	if (ttf_glyph2mesh(&font->glyphs[index], &ret.mesh, TTF_QUALITY_HIGH, TTF_FEATURES_DFLT)
+		!= TTF_DONE)
+		throw std::runtime_error("unable to load glyph mesh");
+
+	return ret;
+}
---
+++ src/Graphics/Font.h
@@ -0,0 +1,31 @@
+
+#pragma once
+
+#include <filesystem>
+#include <ttf2mesh.h>
+
+
+namespace crib
+{
+	class Font
+	{
+	public:
+		Font(const std::filesystem::path& filename);
+		~Font() { ttf_free(font); }
+
+		ttf_t* operator->() { return font; }
+
+		struct Glyph
+		{
+			~Glyph();
+			ttf_glyph_t* glyph = nullptr;
+			ttf_mesh_t* mesh = nullptr;
+		};
+
+		Glyph getGlyph(wchar_t symbol);
+
+	private:
+		ttf_t* font = nullptr;
+	};
+
+}
