ae15e1f16f7445b970b49180e8afae2dd2ce3df4
Hani Kaabi <hani@hnkb.de>
2023-09-26 06:14:30 +0200

    Cleaner float2 semantics

--- examples/hello2d/main.cpp
+++ examples/hello2d/main.cpp
@@ -62,18 +62,18 @@ public:
 		if (ev.type == MouseEvent::Type::ButtonDown)
 		{
 			if (std::isnan(dragStart.x))
-				dragStart = camera.pixelToWorld(toFloat2(ev.pos));
+				dragStart = camera.pixelToWorld((float2)ev.pos);
 		}
 		else if (ev.type == MouseEvent::Type::ButtonUp)
 		{
-			dragStart = { std::numeric_limits<float>::quiet_NaN(), -1 };
+			dragStart = float2::nan();
 			this->draw();
 		}
 		else if (ev.type == MouseEvent::Type::Move && !std::isnan(dragStart.x))
 		{
 			// our mouse pointer is now pointing at a world position, but, we want it to be
 			// pointing to the same position as dragStart => so fix the offset to achieve this
-			camera.view.offset -= dragStart - camera.pixelToWorld(toFloat2(ev.pos));
+			camera.view.offset -= dragStart - camera.pixelToWorld((float2)ev.pos);
 			draw();
 		}
 		else if (ev.type == MouseEvent::Type::Wheel)
@@ -82,11 +82,11 @@ public:
 			// before the scaling. So, I subtract the difference between the new world position
 			// and the original one from the view offset.
 
-			auto before = camera.pixelToWorld(toFloat2(ev.pos));
+			auto before = camera.pixelToWorld((float2)ev.pos);
 
 			camera.view.scale *= powf(1.25f, ev.wheel);
 
-			auto after = camera.pixelToWorld(toFloat2(ev.pos));
+			auto after = camera.pixelToWorld((float2)ev.pos);
 
 			camera.view.offset -= before - after;
 
@@ -95,7 +95,7 @@ public:
 	}
 
 private:
-	float2 dragStart = { std::numeric_limits<float>::quiet_NaN(), -1 };
+	float2 dragStart = float2::nan();
 };
 
 int main()
